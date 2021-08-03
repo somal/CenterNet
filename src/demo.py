c7c0db803cff7262b5169a256d994b572c84c3b4
m.sokolov@businessdecision.ru <Somal1996!gitlab>
2021-08-03 14:03:37 +0300

    Fix current demo

--- src/demo.py
+++ src/demo.py
@@ -1,13 +1,31 @@
 import os
 
 import cv2
+import numpy as np
 
-from src.lib.opts import opts
 from src.lib.detectors.detector_factory import detector_factory
+from src.lib.opts import opts
 
 image_ext = ['jpg', 'jpeg', 'png', 'webp']
 video_ext = ['mp4', 'mov', 'avi', 'mkv']
 time_stats = ['tot', 'load', 'pre', 'net', 'dec', 'post', 'merge']
+
+
+def check_line_inter(line1, line2):
+    img1 = np.zeros(shape=(640, 480), dtype=np.uint8)
+    img2 = np.zeros(shape=(640, 480), dtype=np.uint8)
+    cv2.line(img1, *line1, color=(255, 255, 255), thickness=3)
+    cv2.line(img2, *line2, color=(255, 255, 255), thickness=3)
+    return np.sum(np.logical_and(img1, img2)) > 0
+
+
+def check_intersection_line_and_rect(line, bbox):
+    x1, y1, x2, y2 = bbox[:4]
+    return any([check_line_inter(line, ((x1, y1), (x1, y2))),
+                check_line_inter(line, ((x2, y1), (x2, y2))),
+                check_line_inter(line, ((x1, y1), (x2, y1))),
+                check_line_inter(line, ((x1, y2), (x2, y2))),
+                ])
 
 
 def demo(opt):
@@ -19,16 +37,48 @@ def demo(opt):
     if opt.demo == 'webcam' or \
             opt.demo[opt.demo.rfind('.') + 1:].lower() in video_ext:
 
+        fps = 25
         fourcc = cv2.VideoWriter_fourcc(*'MJPG')
-        out = cv2.VideoWriter('output.mp4', fourcc, 20.0, (640, 480))
+        out = cv2.VideoWriter('output.mp4', fourcc, fps, (640, 480))
 
         cam = cv2.VideoCapture(0 if opt.demo == 'webcam' else opt.demo)
         detector.pause = False
+        lines = [((90, 350), (420, 110)), ((358, 400), (550, 100))]
+
+        min_frames = 10 * fps
+        inter_times_per_line = [[], []]
+        frame_number = 0
         while True:
+            frame_number += 1
             _, img = cam.read()
             img = cv2.resize(img, (640, 480))
             cv2.imshow('input', img)
+            # [cv2.line(img, *line, color=(255, 255, 255), thickness=3) for line in lines]
             img_res, ret = detector.run(img)
+
+            for bbox in ret['results'][2]:
+                if bbox[4] > .3:
+                    rect_bbox = tuple(map(int, bbox[:4]))
+                    for i in range(len(lines)):
+                        if (len(inter_times_per_line[i]) > 0
+                            and frame_number - inter_times_per_line[i][-1] >= min_frames) or \
+                                len(inter_times_per_line[i]) == 0:
+                            # rect_inter = cv2.rotatedRectangleIntersection(rects[i], rect_bbox)
+                            if check_intersection_line_and_rect(lines[i], rect_bbox):
+                                # print(rect_inter)
+                                inter_times_per_line[i].append(frame_number)
+                                print(i, inter_times_per_line)
+
+            rrr = {0: 'L', 1: 'R'}
+            for i in range(len(lines)):
+                frame_diff = (inter_times_per_line[i][-1]) / len(inter_times_per_line[i]) \
+                    if len(inter_times_per_line[i]) else 0
+                cv2.putText(img_res, f'Avg gap time {rrr[i]} {frame_diff / fps:.2f}', (30, 50 * (i + 1)),
+                            fontFace=cv2.FONT_HERSHEY_COMPLEX, fontScale=.75, color=(255, 255, 255))
+            cv2.putText(img_res, f'Count L {len(inter_times_per_line[0])}    R {len(inter_times_per_line[1])}',
+                        (30, 50 * (len(lines) + 1)),
+                        fontFace=cv2.FONT_HERSHEY_COMPLEX, fontScale=.75, color=(255, 255, 255))
+            # print(ret['results'])
             out.write(img_res)
             time_str = ''
             for stat in time_stats:
